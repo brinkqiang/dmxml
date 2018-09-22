1a6e42342498ca57ee862cc1b611d9e1745802f1
brink.qiang <brink.qiang@gmail.com>
2018-09-22 12:15:49 +0800

    add

--- test/dmxmltest.cpp
+++ test/dmxmltest.cpp
@@ -24,27 +24,55 @@ std::string GetXMLDoc()
         xml_node MsgID = Msg.append_child("MsgID");
         MsgID.append_child(node_pcdata).set_value(1);
         xml_node LastTime = Msg.append_child("LastTime");
-        LastTime.append_child(node_pcdata).set_value(1);
+        LastTime.append_child(node_pcdata).set_value(2);
         xml_node MaxTime = Msg.append_child("MaxTime");
-        MaxTime.append_child(node_pcdata).set_value(1);
+        MaxTime.append_child(node_pcdata).set_value(3);
         xml_node AvgTime = Msg.append_child("AvgTime");
-        AvgTime.append_child(node_pcdata).set_value(1);
+        AvgTime.append_child(node_pcdata).set_value(4);
         xml_node TotalTime = Msg.append_child("TotalTime");
-        TotalTime.append_child(node_pcdata).set_value(1);
+        TotalTime.append_child(node_pcdata).set_value(5);
         xml_node MsgCount = Msg.append_child("MsgCount");
-        MsgCount.append_child(node_pcdata).set_value(1);
+        MsgCount.append_child(node_pcdata).set_value(6);
         xml_node TotalMsgSize = Msg.append_child("TotalMsgSize");
-        TotalMsgSize.append_child(node_pcdata).set_value(1);
+        TotalMsgSize.append_child(node_pcdata).set_value(7);
     }
 
     xml_node MsgLast = datas.append_child("MsgLast");
+    MsgLast.append_child(node_pcdata).set_value(8);
     std::stringstream ss;
     doc.save(ss);
     return ss.str();
 }
 
+void SetXMLDoc(const std::string& strDoc)
+{
+    xml_document doc;
+    doc.load_buffer(strDoc.data(), strDoc.size());
+
+    xml_node coder = doc.child("coder");
+    xml_attribute name = coder.attribute("name");
+    xml_attribute version = coder.attribute("version");
+    xml_node datas = coder.child("datas");
+    xml_node Msgs = datas.child("Msgs");
+
+    xml_object_range<xml_named_node_iterator> Msg = Msgs.children("Msg");
+    xml_named_node_iterator It = Msg.begin();
+    for (int i = 0; It != Msg.end(); ++i, ++It)
+    {
+        xml_node MsgID = (*It).child("MsgID");
+        xml_node LastTime = (*It).child("LastTime");
+        xml_node MaxTime = (*It).child("MaxTime");
+        xml_node AvgTime = (*It).child("AvgTime");
+        xml_node TotalTime = (*It).child("TotalTime");
+        xml_node MsgCount = (*It).child("MsgCount");
+        xml_node TotalMsgSize = (*It).child("TotalMsgSize");
+    }
+    xml_node MsgLast = datas.child("MsgLast");
+}
+
 int main( int argc, char* argv[] ) {
     std::string strDoc = GetXMLDoc();
     std::cout << strDoc;
+    SetXMLDoc(strDoc);
     return 0;
 }
